dec6a2ebac12a009a95d6bcd34d51710bcd09095
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-10-05 21:21:22 +0200

    optimizing cover image

--- projects/ngx-notion-cms/src/lib/components/page-cover/page-cover.component.ts
+++ projects/ngx-notion-cms/src/lib/components/page-cover/page-cover.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, input, OnInit, signal } from '@angular/core';
 import { NotionBlock } from '../../types';
 import { getBlockImageURL } from '../../utils/utils';
-
+import { NgOptimizedImage } from '@angular/common'
 @Component({
   selector: 'ngx-page-cover',
   standalone: true,
   imports: [
     CommonModule,
+    NgOptimizedImage
   ],
   templateUrl: './page-cover.component.html',
   styleUrl: './page-cover.component.css',

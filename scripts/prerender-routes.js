ce8f7ed584af52d76253a594e6f3f1e4b0a8bbe9
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-10-05 21:08:20 +0200

    enabling ssg only for test purposes

---
+++ scripts/prerender-routes.js
@@ -0,0 +1,23 @@
+
+(async () => {
+
+
+    const fs = require('fs')
+
+
+
+    //prov, pass environment
+    const id = 'e9c95945794e462d92fe07e34d26b368'
+    const defaultPages = ['/about', '/projects', '/posts']
+    let fileContent = defaultPages.join('\n')
+
+    const postPages = await fetch(`https://notion-api.splitbee.io/v1/table/${id}`).then(res => res.json())
+
+
+    fileContent += '\n'
+    fileContent += postPages.map(resp => `/posts/${resp.id}`).join('\n')
+
+    console.log('routes.txt created 🚀', fileContent)
+    fs.writeFileSync('routes.txt', fileContent)
+
+})()

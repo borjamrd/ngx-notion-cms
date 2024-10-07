919bac302be6df8e62d1756be1dc583d0c0bb84d
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-10-07 18:30:31 +0200

    chore: github link added and defer without placeholder

--- projects/ngx-notion-testbed-app/src/app/app.component.ts
+++ projects/ngx-notion-testbed-app/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { ToggleButtonComponent } from './components/toggle-button/toggle-button.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { GithubComponent } from "./components/github/github.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [NavComponent, RouterOutlet, ToggleButtonComponent, LogoComponent],
+    imports: [NavComponent, RouterOutlet, ToggleButtonComponent, LogoComponent, GithubComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })

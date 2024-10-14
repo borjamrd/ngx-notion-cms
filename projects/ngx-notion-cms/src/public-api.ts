7460bfdbae6afd1df955697da5a7f6fecc6522ba
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-10-14 21:51:02 +0200

    export database item

--- projects/ngx-notion-cms/src/public-api.ts
+++ projects/ngx-notion-cms/src/public-api.ts
@@ -4,6 +4,7 @@
 
 export * from './lib/components/database/notion-database.component';
 export * from './lib/components/notion-page/notion-page.component';
+export * from './lib/components/database-item/database-item.component'
 export * from './lib/services/notion.service';
 export * from './lib/types';
 export * from './lib/providers'

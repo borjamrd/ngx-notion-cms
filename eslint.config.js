67c44e5aed57bdc8f828e7fbcbc4c0a97582e563
borjamrd <borjamrd1@gmail.com>
2026-02-04 10:41:19 +0100

    chore: enhance ESLint configuration for TypeScript and HTML files, update pre-commit hook

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,38 @@ const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
 
+// Helper to add files restriction to each config object
+const withFiles = (configs, files) =>
+    configs.map((config) => ({
+        ...config,
+        files,
+    }));
+
 module.exports = tseslint.config(
+    // TypeScript files
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                projectService: true,
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+        },
+    },
+    ...withFiles([eslint.configs.recommended], ['**/*.ts']),
+    ...withFiles(tseslint.configs.recommended, ['**/*.ts']),
+    ...withFiles(tseslint.configs.stylistic, ['**/*.ts']),
+    ...withFiles(angular.configs.tsRecommended, ['**/*.ts']),
     {
         files: ['**/*.ts'],
         plugins: {
             '@typescript-eslint': tseslint.plugin,
         },
-        extends: [
-            eslint.configs.recommended,
-            ...tseslint.configs.recommended,
-            ...tseslint.configs.stylistic,
-            ...angular.configs.tsRecommended,
-        ],
         processor: angular.processInlineTemplates,
         rules: {
-            // Ahora ya no debería dar error aquí
             '@typescript-eslint/no-explicit-any': 'off',
             '@angular-eslint/directive-selector': [
                 'error',
@@ -29,12 +46,11 @@ module.exports = tseslint.config(
             ],
         },
     },
+    // HTML templates
+    ...withFiles(angular.configs.templateRecommended, ['**/*.html']),
+    ...withFiles(angular.configs.templateAccessibility, ['**/*.html']),
     {
         files: ['**/*.html'],
-        extends: [
-            ...angular.configs.templateRecommended,
-            ...angular.configs.templateAccessibility,
-        ],
         rules: {
             '@angular-eslint/template/click-events-have-key-events': ['warn'],
             '@angular-eslint/template/interactive-supports-focus': ['warn'],

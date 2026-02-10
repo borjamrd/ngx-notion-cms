3618a5cb39c23388b477bb5e47d6363a9a8a6a0e
borjamrd <borjamrd1@gmail.com>
2026-02-10 16:42:25 +0100

    refactor(notionBlock): no input renames

--- projects/ngx-notion-cms/src/lib/components/notion-block/notion-block.component.ts
+++ projects/ngx-notion-cms/src/lib/components/notion-block/notion-block.component.ts
@@ -1,5 +1,12 @@
-
-import { ChangeDetectionStrategy, Component, effect, ElementRef, HostBinding, inject, input } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    effect,
+    ElementRef,
+    HostBinding,
+    inject,
+    input,
+} from '@angular/core';
 import { Nl2brPipe } from '../../pipes/nl2br.pipe';
 import { SecureResourceUrlPipe } from '../../pipes/safe-resource-url.pipe';
 import { NotionBlock } from '../../types/block.type';
@@ -11,27 +18,26 @@ import { PageCoverComponent } from '../page-cover/page-cover.component';
 @Component({
     selector: 'ngx-notion-block',
     imports: [
-    NotionBlockTextComponent,
-    Nl2brPipe,
-    SecureResourceUrlPipe,
-    NotionBlockCodeComponent,
-    PageCoverComponent,
-    NotionBlockCalloutComponent
-],
+        NotionBlockTextComponent,
+        Nl2brPipe,
+        SecureResourceUrlPipe,
+        NotionBlockCodeComponent,
+        PageCoverComponent,
+        NotionBlockCalloutComponent,
+    ],
     templateUrl: './notion-block.component.html',
     styleUrl: './notion-block.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotionBlockComponent {
     templateRef = inject(ElementRef);
-    notionBlockSignal = input.required<NotionBlock>({ 'alias': 'notionBlock' });
+    notionBlockSignal = input.required<NotionBlock>();
     previousBlockType = input<NotionBlock['type']>();
     numberedListPosition = 1;
     @HostBinding('id')
     blockId: string = '';
-    
+
     blockIdUpdateEffectRef = effect(() => {
         this.blockId = this.notionBlockSignal().id;
-    })
-
+    });
 }
